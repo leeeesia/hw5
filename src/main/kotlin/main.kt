e5b6fcc8dc6f36e496b38a791e3e0c888bc749a7
Alesya Hitrova <alesyahitrova@yandex.ru>
2023-02-06 20:02:13 +0300

    pz6

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,8 +1,11 @@
 fun main() {
-    val post1 = Post(text = "post 1", date = 12, likes = likes(50, true))
-    val post2 = Post(text = "post 2", date = 34, likes = likes(23, true))
-    val post3 = Post(text = "post 3", date = 78, likes = likes(67, true))
-    val post4 = Post(id = 2, text = "new post 2", date = 56, likes = likes(45, true))
+    val typePhoto = Photo("photo")
+    val typeVideo = Video("video")
+    val attachments = mapOf("photo" to typePhoto, "video" to typeVideo)
+    val post1 = Post(text = "post 1", date = 12, likes = likes(50, true), attachments = attachments)
+    val post2 = Post(text = "post 2", date = 34, likes = likes(23, true), attachments = attachments)
+    val post3 = Post(text = "post 3", date = 78, likes = likes(67, true), attachments = attachments)
+    val post4 = Post(id = 2, text = "new post 2", date = 56, likes = likes(45, true), attachments = attachments)
     WallService.add(post1)
     WallService.add(post2)
     WallService.add(post3)
@@ -14,15 +17,31 @@ data class Post(
     val id: Int = 0,
     val ownerId: Int = 0,
     val fromId: Int = 0,
+    val createdBy: Int = 0,
     val date: Int,
     val text: String = "",
-    val friendsOnly: Boolean = false,
-    val postType: String = "post",
-    val isPinned: Boolean = false,
-    val isFavorite: Boolean = false,
-    val canPin: Boolean = true,
-    val likes: likes
+    val replyPostId: Boolean? = null,
+    val friendsOnly: Boolean? = false,
+    val postType: String? = "post",
+    val canEdit: Boolean = false,
+    val isPinned: Boolean? = null,
+    val markedAsAds: Boolean = false,
+    val isFavorite: Boolean? = null,
+    val canPin: Boolean? = true,
+    val postponedId: Int = 0,
+    val likes: likes,
+    val attachments : Map<String, Attachments>
 )
+
+interface Attachments{
+    val type : String
+}
+
+class Photo (override val type: String = "photo" ) : Attachments
+class Video (override val type: String = "video" ) : Attachments
+class Audio (override val type: String = "audio" ) : Attachments
+class Doc (override val type: String = "doc" ) : Attachments
+class Note (override val type: String = "note" ) : Attachments
 
 class likes(
     val count: Int,
