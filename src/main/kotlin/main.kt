44238afcfc4ccc682561016f174565fe6b17f6b0
Alesya Hitrova <alesyahitrova@yandex.ru>
2023-02-07 23:59:48 +0300

    pz7

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,14 +2,18 @@ fun main() {
     val typePhoto = Photo("photo")
     val typeVideo = Video("video")
     val attachments = mapOf("photo" to typePhoto, "video" to typeVideo)
-    val post1 = Post(text = "post 1", date = 12, likes = likes(50, true), attachments = attachments)
-    val post2 = Post(text = "post 2", date = 34, likes = likes(23, true), attachments = attachments)
-    val post3 = Post(text = "post 3", date = 78, likes = likes(67, true), attachments = attachments)
-    val post4 = Post(id = 2, text = "new post 2", date = 56, likes = likes(45, true), attachments = attachments)
+    val post1 = Post(text = "post 1", date = 12, likes = Likes(50, true), attachments = attachments)
+    val post2 = Post(text = "post 2", date = 34, likes = Likes(23, true), attachments = attachments)
+    val post3 = Post(text = "post 3", date = 78, likes = Likes(67, true), attachments = attachments)
+    val post4 = Post(id = 2, text = "new post 2", date = 56, likes = Likes(45, true), attachments = attachments)
+    val comment1 = Comment(date = 34, text = "com 1", attachments = attachments)
+    val comment2 = Comment(date = 34, text = "com 2", attachments = attachments)
     WallService.add(post1)
     WallService.add(post2)
     WallService.add(post3)
     WallService.update(post4)
+    WallService.createComment(2, comment1)
+    WallService.createComment(3, comment2)
     WallService.printAll()
 }
 
@@ -29,33 +33,58 @@ data class Post(
     val isFavorite: Boolean? = null,
     val canPin: Boolean? = true,
     val postponedId: Int = 0,
-    val likes: likes,
-    val attachments : Map<String, Attachments>
+    val likes: Likes,
+    val attachments: Map<String, Attachments>
 )
 
-interface Attachments{
-    val type : String
+interface Attachments {
+    val type: String
 }
 
-class Photo (override val type: String = "photo" ) : Attachments
-class Video (override val type: String = "video" ) : Attachments
-class Audio (override val type: String = "audio" ) : Attachments
-class Doc (override val type: String = "doc" ) : Attachments
-class Note (override val type: String = "note" ) : Attachments
+class Photo(override val type: String = "photo") : Attachments
+class Video(override val type: String = "video") : Attachments
+class Audio(override val type: String = "audio") : Attachments
+class Doc(override val type: String = "doc") : Attachments
+class Note(override val type: String = "note") : Attachments
 
-class likes(
+class PostNotFoundException(message: String) : RuntimeException(message)
+
+class Likes(
     val count: Int,
     val userLikes: Boolean
 )
 
+data class Comment(
+    val id: Int = 0,
+    val fromId: Int = 0,
+    val date: Int,
+    val text: String = "",
+    val replyToUser: Int? = null,
+    val replyToComment: Int? = null,
+    val attachments: Map<String, Attachments>
+)
+
 object WallService {
     private var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
     private var postId = 0
 
     fun add(post: Post): Post {
         postId++
         posts += post.copy(id = postId)
         return posts.last()
+    }
+
+    fun createComment(postId: Int, comment: Comment): Comment {
+        var count: Int? = null
+        for ((index, post) in posts.withIndex()) {
+            if (post.id == postId) {
+                comments += comment
+                count = 1
+            }
+        }
+        count ?: throw PostNotFoundException("id not found")
+        return comment
     }
 
     fun update(post: Post): Boolean {
@@ -71,6 +100,9 @@ object WallService {
     fun printAll() {
         for (post in posts) {
             println(post)
+        }
+        for (comment in comments) {
+            println(comment)
         }
     }
 
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -10,21 +10,36 @@ class WallServiceTest {
 
     @Test
     fun add() {
-        val result = (WallService.add(Post(date = 24, text = "post 1", likes = likes(455, true)))).id
+        val result = (WallService.add(Post(date = 24, text = "post 1", likes = Likes(455, true),attachments = emptyMap()))).id
         assertEquals(1, result)
     }
 
     @Test
     fun updateTrue() {
-        WallService.add(Post(date = 24, text = "post 1", likes = likes(455, true)))
-        val result = (WallService.update(Post(id = 1, date = 34, text = "new post 1", likes = likes(45, true))))
+        WallService.add(Post(date = 24, text = "post 1", likes = Likes(455, true),attachments = emptyMap()))
+        val result = (WallService.update(Post(id = 1, date = 34, text = "new post 1", likes = Likes(45, true),attachments = emptyMap())))
         assertTrue(result)
     }
 
     @Test
     fun updateFalse() {
-        WallService.add(Post(date = 24, text = "post 1", likes = likes(455, true)))
-        val result = (WallService.update(Post(id = 4, date = 34, text = "new post 1", likes = likes(467, false))))
+        WallService.add(Post(date = 24, text = "post 1", likes = Likes(455, true),attachments = emptyMap()))
+        val result = (WallService.update(Post(id = 4, date = 34, text = "new post 1", likes = Likes(467, false),attachments = emptyMap())))
         assertFalse(result)
+    }
+
+    @Test
+    fun createCommentTrue() {
+        val comment1 = Comment(date = 23,text = "com 1",attachments = emptyMap())
+        WallService.add(Post(date = 24, text = "post 1", likes = Likes(455, true),attachments = emptyMap()))
+        val result = WallService.createComment(1,comment1)
+        assertEquals(comment1, result)
+    }
+
+    @Test(expected = PostNotFoundException::class)
+    fun createCommentNotFound() {
+        val comment1 = Comment(date = 23,text = "com 1",attachments = emptyMap())
+        WallService.add(Post(date = 24, text = "post 1", likes = Likes(455, true),attachments = emptyMap()))
+        val result = WallService.createComment(4,comment1)
     }
 }
